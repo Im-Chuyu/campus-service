28a3f098af18306f5bad119ad89f9e8a16d6ef67
Im-Chuyu <hai.work.twilight@outlook.com>
2026-06-01 11:00:45 +0800

    更新

--- src/main/java/com/twilight/campus/CampusServiceApplication.java
+++ src/main/java/com/twilight/campus/CampusServiceApplication.java
@@ -1,9 +1,11 @@
 package com.twilight.campus;
 
+//import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@MapperScan("com.twilight.campus.mapper")
 public class CampusServiceApplication {
 
     public static void main(String[] args) {
